a5a4609b3ba697331f2c5e87beb9cbda16fe314a
Ala111205 <sadham070403@gmail.com>
2025-10-29 21:13:02 +0530

    staged

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:6500";
+const API_URL = "https://to-do-list-22jb.onrender.com";
 
 const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
